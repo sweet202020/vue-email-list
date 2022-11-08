562f3064b464e6c2989bb9584b82f8c3330965ab
sweet202020 <dolcealessio1@gmail.com>
2022-11-08 15:04:14 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+/* Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista. */
+
+const{createApp}= Vue
+
+createApp({
+    data(){
+        return{
+            message: 'ciao vue'
+        }
+    }
+}).mount('#app')
